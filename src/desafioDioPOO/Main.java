f45bb008de0be941e3639b946e42e95428e9ba2c
Carla Izabella <carlaizabellajpr@gmail.com>
2022-04-03 12:50:43 -0400

    Mentoria e Curso

--- src/desafioDioPOO/Main.java
+++ src/desafioDioPOO/Main.java
@@ -1,4 +1,32 @@
 package desafioDioPOO;
 
+import desafioDioPOO.dominio.Curso;
+import desafioDioPOO.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args){
+
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso JS");
+        curso2.setDescricao("descrição curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("descrição mentoria Java");
+        mentoria.setDate(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
 }
